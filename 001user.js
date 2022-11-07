cd971557ce066a9aa64c0be858ca3d005cd13db2
javi-antequera <95370033+javi-antequera@users.noreply.github.com>
2022-11-07 13:51:30 +0100

    Terminado 1y 2

---
+++ 001user.js
@@ -0,0 +1,5 @@
+let user=new Object();  
+user.name="Jonh Snow";
+user.surname="Sam Sagaz";
+user.name="Peter Pan";
+delete user.name;
---
+++ 002verifyIfIsEmpty.js
@@ -0,0 +1,12 @@
+function isEmpty(objeto){
+    if(Object.keys(objeto).length===0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "Hora de levantarse";
+alert( isEmpty(schedule) ); // false
