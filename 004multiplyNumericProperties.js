b7a3374e5445396aa79317274c09bc453a78e0a2
javi-antequera <95370033+javi-antequera@users.noreply.github.com>
2022-11-08 08:48:49 +0100

    4 terminado

--- 004multiplyNumericProperties.js
+++ 004multiplyNumericProperties.js
@@ -3,6 +3,19 @@ let menu = {
     height: 300,
     title: "Mi menú"
   };
+  mostrarMenu(menu);
+  multiplyNumeric(menu);
+  mostrarMenu(menu);
   function multiplyNumeric(menu){
-    
+    for (let propiedad in menu){
+      if(typeof menu[propiedad]=="number"){
+        menu[propiedad]=menu[propiedad]*2;
+      }
+    }
+  }
+  //Funcion que muestra todos los dtos del menú para comprobar los cambios
+  function mostrarMenu(menu){
+    for (let propiedad in menu){
+      alert(menu[propiedad]);
+    }
   }
