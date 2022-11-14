3f0a950cc2878a3bee8ac85550e14eb5e9deba5f
DAW <DAW@ILERNA.SEVILLA>
2022-11-14 14:10:44 +0100

    Terminado y comentado

--- 027transformSnakeToCamel.js
+++ 027transformSnakeToCamel.js
@@ -1,16 +1,16 @@
 function camelice(str){
-    let array=str.split("");
+    let array=str.split("");//Divido el string en un array de char
     let array2=[];//Array para guardar el string sin las _
     let mayus=false;//Booleana de control para asignar las mayusculas
     for (let element of array) {
-        if(element!="_"){
-            if(mayus==true){
-                array2.push(element.toUpperCase());
-                mayus=false;
-            }else{
+        if(element!="_"){//Si el elemento del array no es _
+            if(mayus==true){//Si el ultimo elemento era _
+                array2.push(element.toUpperCase());//Lo meto a mi array en mayusculas
+                mayus=false;//Devuelvo el valor del controlador a false
+            }else{//Si no meto el elemento sin cambios
                 array2.push(element);
             } 
-        }else{
+        }else{//Si el elemento es _ cambio el valor del controlador a true
             mayus=true;
         }
     }
