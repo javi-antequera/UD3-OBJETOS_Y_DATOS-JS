3b772ec6909bfd7658fbf7924b304f9be6384235
DAW <DAW@ILERNA.SEVILLA>
2022-11-14 14:20:27 +0100

    En proceso

---
+++ 028mapToName.js
@@ -0,0 +1,6 @@
+let acdc= { name: "ACDC", age: 25 };
+let metallica = { name: "Metallica", age: 30 };
+let guns= { name: "Guns and Roses", age: 28 };
+let bands = [ acdc, metallica, guns];
+let names = /* ... tu código */
+alert( names ); 
